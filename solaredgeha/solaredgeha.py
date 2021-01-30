131eab44759f5f41f6302cace087a8c3ee30dc66
Aaron McCarthy <mccarthy.aaron@gmail.com>
2021-01-30 17:38:59 +1000

    Change to use credentials for auth
    Use username/password instead of session cookie extracted from browser.

--- solaredgeha/solaredgeha.py
+++ solaredgeha/solaredgeha.py
@@ -1,5 +1,8 @@
 import requests
+from xml.etree import ElementTree
 
+
+LOGINURL = 'https://api.solaredge.com/solaredge-apigw/api'
 BASEURL = 'https://ha.monitoring.solaredge.com/api/homeautomation/v1.0'
 
 COOKIE_NAME = "SPRING_SECURITY_REMEMBER_ME_COOKIE"
@@ -10,20 +13,97 @@ class SolaredgeHa(object):
     Object containing SolarEdge's Home Automation site API-methods.
     """
 
-    def __init__(self, site, site_token):
+    def __init__(self, username, password):
         """
         To communicate, you need to set a site and site token.
         Get it from your account.
 
         Parameters
         ----------
-        site : str
-        site_token : str
+        username : str
+        password : str
         """
-        self.site = site
-        self.token = site_token
+        self.username = username
+        self.password = password
 
-    def get_devices(self):
+        self.sites = []
+        self.token = None
+
+    def login(self):
+        """
+        Login to service
+
+        Returns
+        -------
+        bool
+        """
+
+        url = urljoin(LOGINURL, "login")
+
+        params = {
+            "j_username": self.username,
+            "j_password": self.password
+        }
+
+        r = requests.post(url, data=params, allow_redirects=False)
+
+        r.raise_for_status()
+
+        if (r.status_code != 302):
+            return False
+
+        self.token = r.cookies.get(COOKIE_NAME)
+
+        return self.updateSites()
+
+    def updateSites(self):
+        """
+        Update available sites
+
+        Returns
+        -------
+        bool
+        """
+
+        if (self.token == None):
+            return False
+
+        url = urljoin(LOGINURL, "fields", "list")
+
+        cookies = {
+            COOKIE_NAME: self.token
+        }
+
+        r = requests.get(url, cookies=cookies)
+
+        r.raise_for_status()
+
+        tree = ElementTree.fromstring(r.content)
+
+        self.sites = []
+        for id in tree.iter('id'):
+            self.sites.append(id.text)
+
+        return True
+
+    def ensureSession(self):
+        if (self.token == None):
+            self.login()
+
+        return self.token != None
+
+    def get_sites(self):
+        """
+        Get list of site ids
+
+        Returns
+        -------
+        list
+        """
+
+        return self.sites
+
+    def get_devices(self, site):
         """
         Request devices
 
@@ -32,9 +112,14 @@ class SolaredgeHa(object):
         dict
         """
 
-        url = urljoin(BASEURL, "sites", self.site, "devices")
+        if (self.ensureSession() == False):
+            return None
 
-        cookies = dict(SPRING_SECURITY_REMEMBER_ME_COOKIE=self.token)
+        url = urljoin(BASEURL, "sites", site, "devices")
+
+        cookies = {
+            COOKIE_NAME: self.token
+        }
 
         r = requests.get(url, cookies=cookies)
         r.raise_for_status()
@@ -49,18 +134,20 @@ class SolaredgeHa(object):
         dict
         """
 
-        url = urljoin(BASEURL, self.site, "devices", reporterId, "activationState")
+        if (self.ensureSession() == False):
+            return None
 
-        cookies = dict(SPRING_SECURITY_REMEMBER_ME_COOKIE=self.token)
+        url = urljoin(BASEURL, self.sites[0], "devices", reporterId, "activationState")
+
+        cookies = {
+            COOKIE_NAME: self.token
+        }
 
         params = {
             "mode": "MANUAL",
             "level": level,
             "duration": duration
         }
-
-        print("Params:")
-        print(params)
 
         r = requests.put(url, json=params, cookies=cookies)
         r.raise_for_status()
